2afcfe8db101b1452df7065a0368ca5f04f7b5ac
mikbolshakov <107908985+mikbolshakov@users.noreply.github.com>
2024-06-03 15:01:06 +0300

    tested and deployed RunnerOmni

---
+++ deploy/deployRunnerOmni.ts
@@ -0,0 +1,37 @@
+import { ethers } from 'hardhat';
+import hre from 'hardhat';
+
+// npx hardhat run deploy/deployRunnerOmni.ts --network scroll_sepolia
+async function main() {
+  const adminAddress = '0x6ae19a226A6Cec3E29D5dfC90C2bd6640d8d77b9';
+  const scrollAndLineaEndpointTestnet =
+    '0x6EDCE65403992e310A62460808c4b910D972f10f';
+    
+
+  const Runner2060 = await ethers.getContractFactory('RunnerOmni');
+  const runner2060 = await Runner2060.deploy(
+    adminAddress,
+    scrollAndLineaEndpointTestnet,
+    adminAddress,
+  );
+
+  await runner2060.deployed();
+  console.log(`RunnerOmni deployed to ${runner2060.address}`);
+
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+
+  await hre.run('verify:verify', {
+    address: runner2060.address,
+    constructorArguments: [
+      adminAddress,
+      scrollAndLineaEndpointTestnet,
+      adminAddress,
+    ],
+    contract: 'contracts/RunnerOmni.sol:RunnerOmni',
+  });
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,28 +1,55 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
+import '@layerzerolabs/toolbox-hardhat';
 import 'solidity-coverage';
+import 'hardhat-deploy';
 import dotenv from 'dotenv';
 dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: '0.8.24',
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.24',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
   networks: {
     linea_sepolia: {
       url: process.env.LINEA_SEPOLIA as string,
+      accounts: [process.env.LINEA_SEPOLIA_ADMIN_PRIVATE_KEY as string],
+    },
+    scroll_sepolia: {
+      url: 'https://sepolia-rpc.scroll.io/',
       accounts: [process.env.LINEA_SEPOLIA_ADMIN_PRIVATE_KEY as string],
     },
   },
   etherscan: {
     apiKey: {
       linea_sepolia: process.env.LINEASCAN_API_KEY as string,
+      scroll_sepolia: process.env.SCROLLSCAN_API_KEY as string,
     },
     customChains: [
       {
-        network: "linea_sepolia",
+        network: 'linea_sepolia',
         chainId: 59141,
         urls: {
-          apiURL: "https://api-sepolia.lineascan.build/api",
-          browserURL: "https://sepolia.lineascan.build",
+          apiURL: 'https://api-sepolia.lineascan.build/api',
+          browserURL: 'https://sepolia.lineascan.build',
+        },
+      },
+      {
+        network: 'scroll_sepolia',
+        chainId: 534351,
+        urls: {
+          apiURL: 'https://api-sepolia.scrollscan.com/api',
+          browserURL: 'https://sepolia.scrollscan.build',
         },
       },
     ],
